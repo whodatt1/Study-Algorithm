76e68b865eb16d10feb3a3f85d20481eb2f65cf7
whodatt1 <99710312+whodatt1@users.noreply.github.com>
2022-12-07 18:38:13 +0900

    입출력 문제

---
+++ Baekjoon/입출력/2557.java
@@ -0,0 +1,18 @@
+/* 
+
+-문제
+Hello World!를 출력하시오.
+
+-입력
+없음
+
+-출력
+Hello World!를 출력하시오. 
+
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
