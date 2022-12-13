90d54d389cbfc46a72cb214dcdbb09ce75e14e3c
whodatt1 <99710312+whodatt1@users.noreply.github.com>
2022-12-13 19:39:56 +0900

    입출력 문제

---
+++ Baekjoon/입출력/11720.java
@@ -0,0 +1,30 @@
+/*
+
+-문제
+N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+-입력
+첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+
+-출력
+입력으로 주어진 숫자 N개의 합을 출력한다.
+
+*/
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String a = sc.next();
+		sc.close();
+		
+		int sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+			sum += a.charAt(i) - '0';
+		}
+		System.out.println(sum);
+	}
+};
