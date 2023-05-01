972bfc7c590a3e6d2a6bb8d23cdd6b2421f11069
whodatt1 <99710312+whodatt1@users.noreply.github.com>
2023-05-01 15:24:19 +0900

    문자열 문제

---
+++ Baekjoon/문자열/9251.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/*
+
+LCS
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+0.1 초 (하단 참고)	256 MB	68999	28056	20587	40.230%
+문제
+LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
+
+예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
+
+입력
+첫째 줄과 둘째 줄에 두 문자열이 주어진다. 문자열은 알파벳 대문자로만 이루어져 있으며, 최대 1000글자로 이루어져 있다.
+
+출력
+첫째 줄에 입력으로 주어진 두 문자열의 LCS의 길이를 출력한다.
+
+예제 입력 1
+ACAYKP
+CAPCAK
+예제 출력 1
+4
+
+ */
+
+public class Main {
+
+	static char[] str1;
+	static char[] str2;
+
+	static int[][] dp;
+
+	public static int LCS(int x, int y) {
+		if (x == -1 || y == -1) {
+			return 0;
+		}
+
+		if (dp[x][y] == 0) {
+
+			if (str1[x] == str2[y]) {
+				dp[x][y] = LCS(x - 1, y - 1) + 1;
+			} else {
+				dp[x][y] = Math.max(LCS(x - 1, y), LCS(x, y - 1));
+			}
+
+		}
+
+		return dp[x][y];
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		str1 = br.readLine().toCharArray();
+		str2 = br.readLine().toCharArray();
+
+		dp = new int[str1.length][str2.length];
+
+		System.out.println(LCS(str1.length - 1, str2.length - 1));
+    }
+}
